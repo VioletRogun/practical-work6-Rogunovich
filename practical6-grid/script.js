e5cb3c320073576fa9e2ee0ece2375f718229022
Veta <viollarog599@gmail.com>
2024-05-15 11:18:36 +0300

    pw6_complete_

---
+++ practical6-grid/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menu = document.querySelector('.nav');
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+}
